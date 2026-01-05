5da960e25a79ea231c05323cf99a38a568258c88
Ziba20 <zibachowdhury20@gmail.com>
2026-01-05 23:00:47 +0000

    4th

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function updatePlayerName() {
-var title1 = document.getElementById("fplayer");
-var title2 = title1;
+    let title1 = document.getElementById("fplayer");
+    let title2 = title1;
 
 document.querySelector(".bEnter").addEventListener("click", function() {
-    const name1 = document.getElementById("playerName");
     window.location.href = "about.html";
-    name1.textContent = "Welcome Player: " + title2 + "!";
+    localStorage.setItem("playerName", title2.value);
 
 });
 }
