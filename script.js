fd449f31f70b9b7fdb1455f3108a8c6d0d6cf007
Ziba20 <zibachowdhury20@gmail.com>
2026-01-12 17:42:49 +0000

    5th

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function updatePlayerName() {
-    let title1 = document.getElementById("fplayer");
-    let title2 = title1;
 
-document.querySelector(".bEnter").addEventListener("click", function() {
-    window.location.href = "about.html";
-    localStorage.setItem("playerName", title2.value);
-
-});
-}
 
 // document.getElementById("playerName").textContent = "Welcome Player: " + title2 + "!";
+
+//function updatePlayerName() {
+   // let title1 = document.getElementById("fplayer");
+   // let title2 = title1;
+
+// document.querySelector(".bEnter").addEventListener("click", function() {
+ //   window.location.href = "about.html";
+   //  localStorage.setItem("playerName", title2.value);
+
+//});
+// }
